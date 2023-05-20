17ecf14ce0c9e36c091306dc61900b47393af239
Davi <davicamiloribeiro@gmail.com>
2023-05-20 16:30:34 -0300

    Base do projeto

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const bodyParser = require("body-parser"); //Importando Body Parser
+const connection = require("./database/database");
+const adminAuth = require("./middleware/adminAuth");
+
+const usersController = require("./controllers/UsersController");
+
+// View engine
+app.set('view engine','ejs');
+
+//Sessions
+app.use(session({
+    secret: "youshallnotpass", cookie: { maxAge: 14400000},
+    saveUninitialized: true,
+    resave: true
+}))
+
+//Arquivos estaticos
+app.use(express.static('public'));
+app.use('/public', express.static('public'));
+app.use('/public/img/', express.static('./public/img'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Database
+connection.authenticate()
+    .then(()=>{
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);
+    })
+
+app.use("/", usersController);
+
+// Router
+app.get("/", adminAuth, (req, res) => {
+    res.render("index.ejs");
+})
+
+// End Router
+app.listen(3000, () => {
+    console.log("O servidor está rodando!")
+})
+
+
+
