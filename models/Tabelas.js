facf43258c57ec66863464fd0914f14850ba9153
Davi <davicamiloribeiro@gmail.com>
2023-05-23 17:24:58 -0300

    Model de Tabela

---
+++ models/Tabelas.js
@@ -0,0 +1,64 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const Campeonato = require("./Campeonatos");
+const Time = require("./Times");
+
+const Tabela = connection.define('tabelas',{
+    colocacao:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    pontos:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    jogos:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    vitorias:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    empates:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    derrotas:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    porcentagem:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    golsPro:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    golsContra:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    ativo:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+})
+
+Tabela.belongsTo(Campeonato);
+Tabela.belongsTo(Time);
+
+Tabela.sync({force:false});
+
+module.exports = Tabela;
