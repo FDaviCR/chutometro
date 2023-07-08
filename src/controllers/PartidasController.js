efe84aecad18270e99948b132a209f14b0214134
Davi <davicamiloribeiro@gmail.com>
2023-07-08 20:06:39 -0300

    Listagem de partidas

--- src/controllers/PartidasController.js
+++ src/controllers/PartidasController.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { QueryTypes } = require('sequelize');
+const connection = require('../database/database');
 
 const router = express.Router();
 // const bcrypt = require('bcryptjs');
-const Partidas = require('../models/Partidas');
+// const Partidas = require('../models/Partidas');
+// const Times = require('../models/Times');
 
-router.get('/partidas', (req, res) => {
-    res.render('admin/partidas/index');
+/** ROTAS DE RENDERIZAÇÃO DE PÁGINAS */
+router.get('/partidas', async (req, res) => {
+    const partidas = await connection.query('SELECT partidas.id, partidas.numeroPartida, m.time as Mandante, partidas.golsMandante, partidas.golsVisitante, v.time as Visitante FROM partidas INNER JOIN times AS m ON m.id = partidas.mandanteId INNER JOIN times AS v ON v.id = partidas.visitanteId', { type: QueryTypes.SELECT });
+
+    res.render('public/partidas/index', { partidas });
 });
+
+/** FUNÇÕES DE PROCESSAMENTO DE DADOS */
 
 module.exports = router;
