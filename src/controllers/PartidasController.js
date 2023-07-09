2714b6bf8cfc51ebd2d4d0275b338f3d1b78b259
Davi <davicamiloribeiro@gmail.com>
2023-07-09 15:23:37 -0300

    Listagem de jogos por campeonato e por rodada

--- src/controllers/PartidasController.js
+++ src/controllers/PartidasController.js
@@ -14,6 +14,22 @@ router.get('/partidas', async (req, res) => {
     res.render('public/partidas/index', { partidas });
 });
 
+router.get('/partidas/campeonato/:idCampeonato', async (req, res) => {
+    const edicao = req.params.idCampeonato;
+    const partidas = await connection.query('SELECT partidas.id, partidas.numeroPartida, m.time as Mandante, partidas.golsMandante, partidas.golsVisitante, v.time as Visitante FROM partidas INNER JOIN times AS m ON m.id = partidas.mandanteId INNER JOIN times AS v ON v.id = partidas.visitanteId WHERE campeonatoId = :edicao', { replacements: { edicao }, type: QueryTypes.SELECT });
+
+    res.render('public/partidas/index', { partidas });
+});
+
+router.get('/partidas/campeonato/:idCampeonato/:rodada', async (req, res) => {
+    const campeonato = req.params.idCampeonato;
+    const { rodada } = req.params;
+
+    const partidas = await connection.query(`SELECT partidas.id, partidas.numeroPartida, m.time as Mandante, partidas.golsMandante, partidas.golsVisitante, v.time as Visitante FROM partidas INNER JOIN times AS m ON m.id = partidas.mandanteId INNER JOIN times AS v ON v.id = partidas.visitanteId WHERE campeonatoId = ${campeonato} and rodada = ${rodada}`, { type: QueryTypes.SELECT });
+
+    res.render('public/partidas/index', { partidas });
+});
+
 /** FUNÇÕES DE PROCESSAMENTO DE DADOS */
 
 module.exports = router;
