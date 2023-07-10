a56eaf03566cebfff77e162d654729db4a152b81
Davi <davicamiloribeiro@gmail.com>
2023-07-10 20:51:32 -0300

    Model de Tabela de Palpites

---
+++ src/models/TabelaPalpites.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const CampeonatoPalpite = require('./CampeonatoPalpites');
+const Usuario = require('./Usuarios');
+
+const TabelaPalpites = connection.define('TabelaPalpites', {
+    pontuacao: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0,
+    },
+    rodada: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0,
+    },
+    ativo: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+}, {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
+});
+
+TabelaPalpites.belongsTo(CampeonatoPalpite);
+TabelaPalpites.belongsTo(Usuario);
+
+TabelaPalpites.sync({ force: false });
+
+module.exports = TabelaPalpites;
