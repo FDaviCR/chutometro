bf24d16f2925cba48d788abf6ac0dceb68575338
Davi <davicamiloribeiro@gmail.com>
2023-07-10 20:13:58 -0300

    Model de Palpite

---
+++ src/models/Palpite.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const Partida = require('./Partidas');
+const Usuario = require('./Usuarios');
+
+const Palpite = connection.define('Palpites', {
+    palpite: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    resultado: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+    valido: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+}, {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
+});
+
+Palpite.belongsTo(Partida);
+Palpite.belongsTo(Usuario);
+
+Palpite.sync({ force: false });
+
+module.exports = Palpite;
