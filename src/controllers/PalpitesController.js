239956774111e87e9eeb9f4351b21a4b9cc2cdf8
Davi <davicamiloribeiro@gmail.com>
2023-08-03 20:20:40 -0300

    Enviar dados do palpite para o backend

--- src/controllers/PalpitesController.js
+++ src/controllers/PalpitesController.js
@@ -4,15 +4,15 @@ const connection = require('../database/database');
 
 const router = express.Router();
 // const bcrypt = require('bcryptjs');
-// const Palpites = require('../models/Palpites');
+const Palpites = require('../models/Palpites');
 
-router.get('/palpites', async (req, res) => {
+router.get('/palpites/:campeonato/:campeonatoPalpites/:jogador/:rodada', async (req, res) => {
     const campeonatoPalpite = req.params.campeonatoPalpites;
     const { rodada, jogador, campeonato } = req.params;
 
     const partidas = await connection.query(
         `
-            SELECT p.id, p.numeroPartida, m.time as Mandante, p.golsMandante, p.golsVisitante, v.time as Visitante, p.partidaRealizada
+            SELECT p.id, p.numeroPartida, m.time as Mandante, m.id as idMandante, p.golsMandante, p.golsVisitante, v.time as Visitante, v.id as idVisitante, p.partidaRealizada
             FROM partidas AS p
             INNER JOIN times AS m ON m.id = p.mandanteId
             INNER JOIN times AS v ON v.id = p.visitanteId
@@ -21,7 +21,12 @@ router.get('/palpites', async (req, res) => {
         { type: QueryTypes.SELECT },
     );
 
-    res.render('admin/palpites/index', { partidas, campeonatoPalpite, jogador });
+    res.render('public/palpites/index', { partidas, campeonatoPalpite, jogador });
+});
+
+router.post('/palpite/save', async (req, res) => {
+    console.log(req.body);
+    return 0;
 });
 
 module.exports = router;
