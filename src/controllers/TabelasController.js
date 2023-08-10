6d3654fd4639734280a583ec8ac81bcec8f5b18a
Davi <davicamiloribeiro@gmail.com>
2023-08-10 19:34:14 -0300

    Tela de Tabelas de Campeonato

--- src/controllers/TabelasController.js
+++ src/controllers/TabelasController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-// const Tabelas = require('../models/Tabelas');
+const Tabelas = require('../models/Tabelas');
 // const bcrypt = require('bcryptjs');
 
 router.get('/tabelas', (req, res) => {
